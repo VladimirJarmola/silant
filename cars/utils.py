02b2761a161d87ceb9a002441acb870246b03333
Vladimir <vlad.jarmola@gmail.com>
2024-01-26 15:01:33 +0600

    refactoring

--- cars/utils.py
+++ cars/utils.py
@@ -8,12 +8,19 @@ def q_search(query):
     #     str_query = str(query).zfill(4)
     #     return Cars.objects.all().filter(serial_number_vehicle=str_query)
     
-    number_list = [number for number in query.split()]
-    
+    # number_list = [number for number in query.split()]
+    number_list = []
+    for number in query.split():
+        if number.isdigit():
+            number_list.append(number)
+
     q_objects = Q()
 
-    for token in number_list:
-        token_mod = token.zfill(4)
-        q_objects |= Q(serial_number_vehicle=token_mod)
-
-    return Cars.objects.filter(q_objects)
+    if number_list:
+        for token in number_list:
+            if token.isdigit():
+                token_mod = token.zfill(4)
+                q_objects |= Q(serial_number_vehicle=token_mod)
+        return Cars.objects.filter(q_objects)
+    else:
+        return None
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from cars.models import Cars
 
@@ -13,10 +16,13 @@ def car_search(request):
 
     query = request.GET.get('q', None)
 
-    if query:
+    if query and len(query.split()):
         car_search_list = q_search(query)
+    elif not query:
+        messages.warning(request, "Введите заводской номер!")
+        return HttpResponseRedirect(reverse('main:index'))
     else:
-        car_search_list = query
+        car_search_list = None
 
     # print(car_search_list)
     context = {
