103ef098f26e8d2048275605e201dc157281a4f3
Vladimir <vlad.jarmola@gmail.com>
2024-02-24 00:57:10 +0600

    fix car search template and fix UserChangeForm

--- users/forms.py
+++ users/forms.py
@@ -22,6 +22,13 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        user_role = cleaned_data.get("user_role")
+        service_company = cleaned_data.get("service_company")
+        if user_role == "SE" and not service_company:
+            msg = forms.ValidationError("Выберите из какой сервисной компании пользователь!", code="invalid")
+            self.add_error("service_company", msg)
 
     class Meta:
         model = User
